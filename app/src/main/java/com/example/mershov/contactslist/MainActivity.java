19d7b58e7e67a7a79c0c06fb2ee538f10aaa5201
Maxim Ershov <xamonn1@gmail.com>
2016-09-14 20:11:58 +0300

    Initial commit

---
+++ app/src/main/java/com/example/mershov/contactslist/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.mershov.contactslist;
+
+import android.database.Cursor;
+import android.provider.ContactsContract;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView lvContacts;
+    SimpleCursorAdapter scAdapter;
+    Cursor mContacts;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mContacts = getContentResolver().query(
+                ContactsContract.Contacts.CONTENT_URI,
+                new String [] {ContactsContract.Contacts._ID, ContactsContract.Contacts.DISPLAY_NAME},
+                null,//ContactsContract.Contacts.HAS_PHONE_NUMBER + " = ?",
+                null,//new String[] {"1"},
+                ContactsContract.Contacts.DISPLAY_NAME + " ASC"
+        );
+
+        String[] mContactsColumns = {
+                ContactsContract.Contacts.DISPLAY_NAME
+        };
+
+        int[] mContactsListItems = {R.id.tvName};
+
+        scAdapter = new SimpleCursorAdapter(
+                getApplicationContext(),
+                R.layout.item,
+                mContacts,
+                mContactsColumns,
+                mContactsListItems,
+                0
+        );
+
+        lvContacts = (ListView) findViewById(R.id.lvContacts);
+        lvContacts.setAdapter(scAdapter);
+    }
+}
